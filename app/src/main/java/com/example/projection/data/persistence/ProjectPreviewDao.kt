10a32b0f7a1f95613c0b227f6014f02fb1ecc661
Jesse Williams <jessenw3@gmail.com>
2022-03-02 21:36:17 +1300

    pro-17: add room db classes for project preview

---
+++ app/src/main/java/com/example/projection/data/persistence/ProjectPreviewDao.kt
@@ -0,0 +1,17 @@
+package com.example.projection.data.persistence
+
+import androidx.room.*
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+
+@Dao
+interface ProjectPreviewDao {
+    @Query("SELECT * FROM ProjectPreviewRow")
+    fun getAll(): Flowable<List<ProjectPreviewRow>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(project: ProjectPreviewRow): Completable
+
+    @Delete
+    fun delete(project: ProjectPreviewRow)
+}
---
+++ app/src/main/java/com/example/projection/data/persistence/ProjectPreviewRow.kt
@@ -0,0 +1,12 @@
+package com.example.projection.data.persistence
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ProjectPreviewRow(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "author") val author: String?
+)
