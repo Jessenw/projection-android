ca6061a3406160a94f281d14f1a891b193d5145d
Jesse Williams <jessenw3@gmail.com>
2022-03-20 16:37:39 +1300

    pro-23: add database to repository constructor

--- app/src/main/java/com/example/projection/data/repository/ProjectionRepository.kt
+++ app/src/main/java/com/example/projection/data/repository/ProjectionRepository.kt
@@ -1,11 +1,15 @@
 package com.example.projection.data.repository
 
+import androidx.lifecycle.toLiveData
 import com.example.projection.data.api.ProjectionApi
 import com.example.projection.data.model.ProjectPreview
+import com.example.projection.data.persistence.projectpreview.ProjectionDatabase
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class ProjectionRepository {
+class ProjectionRepository(
+    private val database: ProjectionDatabase
+) {
 
     fun requestGroupbuys(): Flowable<List<ProjectPreview>> =
         ProjectionApi.requestApi
@@ -24,4 +28,9 @@ class ProjectionRepository {
             .onErrorReturn { _ ->
                 emptyList()
             }
+
+//    fun requestSaved(): Flowable<List<ProjectPreview>> =
+//        database
+//            .projectPreviews
+//            .getAll()
 }
