71fc04e0e30a66d8652311cd7b2b119502022167
Jesse Williams <jessenw3@gmail.com>
2022-03-20 16:36:54 +1300

    pro-23: add repository to gb index vm

--- app/src/main/java/com/example/projection/view/screen/projectindex/GroupbuyIndexViewModel.kt
+++ app/src/main/java/com/example/projection/view/screen/projectindex/GroupbuyIndexViewModel.kt
@@ -1,16 +1,21 @@
 package com.example.projection.view.screen.projectindex
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.toLiveData
+import com.example.projection.ProjectionApp
 import com.example.projection.data.model.ProjectPreview
 import com.example.projection.data.repository.ProjectionRepository
 import com.example.projection.view.component.viewmodel.ListViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 class GroupbuyIndexViewModel(
-    repository: ProjectionRepository = ProjectionRepository()
-) : ListViewModel, ViewModel() {
+    application: Application
+) : ListViewModel, AndroidViewModel(application) {
+
+    private val repository = getApplication<ProjectionApp>().projectPreviewRepository
 
     override val dataSource: LiveData<List<ProjectPreview>> =
         repository.requestGroupbuys()
