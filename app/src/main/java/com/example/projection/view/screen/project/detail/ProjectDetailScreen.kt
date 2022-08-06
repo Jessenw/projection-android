9df5a4f1dfa683cd329cd845ee94c5497fb2c96f
Jesse Williams <jessenw3@gmail.com>
2022-08-06 23:46:09 +1200

    Use correct text color for html rendering

--- app/src/main/java/com/example/projection/view/screen/project/detail/ProjectDetailScreen.kt
+++ app/src/main/java/com/example/projection/view/screen/project/detail/ProjectDetailScreen.kt
@@ -1,7 +1,5 @@
 package com.example.projection.view.screen.project.detail
 
-import android.text.Editable
-import android.text.Html
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,23 +12,21 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.projection.view.screen.project.detail.viewmodel.GroupbuyDetailViewModelImpl
-import org.xml.sax.XMLReader
 
 @Composable
 fun ProjectShowScreen(
     viewModel: GroupbuyDetailViewModelImpl = hiltViewModel()
 ) {
-    val context = LocalContext.current
     val project = viewModel.dataSource.observeAsState()
 
     project.value?.data?.let {
-        Column() {
+        Column {
             TopAppBar(
                 title = { Text(it.title, maxLines = 1) },
                 backgroundColor = MaterialTheme.colors.secondary
@@ -48,7 +44,8 @@ fun ProjectShowScreen(
 
 @Composable
 fun Html(rawHtml: String) {
-    AndroidView(factory = { context ->
+    val color = MaterialTheme.colors.onSurface
+    AndroidView({ context ->
         TextView(context).apply {
             text = HtmlCompat.fromHtml(
                 rawHtml,
@@ -57,17 +54,8 @@ fun Html(rawHtml: String) {
                 null
             )
         }
+    },
+    update = {
+        it.setTextColor(color.toArgb())
     })
-}
-
-class ProjectDetailTagHandler(): Html.TagHandler {
-    override fun handleTag(
-        opening: Boolean,
-        tag: String?,
-        output: Editable?,
-        xmlReader: XMLReader?
-    ) {
-
-    }
-
 }
